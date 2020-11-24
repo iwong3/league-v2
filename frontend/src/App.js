98495963f747607264b3eaa03dedc899c5778516
Ivan Wong <ivanwong@gatech.edu>
2020-11-23 21:55:53 -0800

    frontend pulls from backend - match history

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            match_history_data: []
+        }
+    }
+
+    componentDidMount() {
+
+        const match_history_url = "http://localhost:5000/match-history"
+        axios.get(match_history_url).then(resp => {
+            this.setState({
+                match_history_data: resp.data
+            })
+        })
+
+    }
+
+    renderMatchHistory = () => {
+
+        let match_history = []
+        for (let i = 0; i < this.state.match_history_data.length; i++) {
+            match_history.push(<div>Champion: {this.state.match_history_data[i]["champion_name"]}</div>)
+            match_history.push(<div>Played At: {this.state.match_history_data[i]["date"]}</div>)
+        }
+        return (
+            <div>{match_history}</div>
+        )
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1> Match History! </h1>
+                {this.state.match_history_data.length ? this.renderMatchHistory() : <none/>}
+            </div>
+        );
+    }
+
+}
